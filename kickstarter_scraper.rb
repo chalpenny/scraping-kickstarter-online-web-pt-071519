b29ce30432ae9f055e6cd903b91ed4b0456f50df
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-28 03:32:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,17 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
+
+#projects: kickstarter.css("li.project.grid_4")
+#title: project.css("h2.bbcard_name strong a").text
+#image link: project.css("div.project-thumbnaila img").attribute("src.").value
+#description: project.css("p.bbcard_blurb").text
+#location: project.css("u1.project-meta").text
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
