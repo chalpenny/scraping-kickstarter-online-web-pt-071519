e8d6eed59e642ae36f1ebca39f4125e93999d622
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-28 03:34:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,9 @@ require 'pry'
 #title: project.css("h2.bbcard_name strong a").text
 #image link: project.css("div.project-thumbnaila img").attribute("src.").value
 #description: project.css("p.bbcard_blurb").text
-#location: project.css("u1.project-meta").text
+#location: project.css("u1.project-meta span.location-name").text
+#percent_funded: project.css("u1.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
